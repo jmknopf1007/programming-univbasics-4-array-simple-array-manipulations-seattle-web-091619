c6efc83c3f44cadccbbef83c6d424cd3bd53e85f
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-18 18:47:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,1 @@
-do colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
-    updated_array = using_push(colors_in_the_rainbow, next_color)
-    expect(updated_array.last).to eq(next_color)
-    
-  end
+countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
