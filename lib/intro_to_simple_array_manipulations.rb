f1a4d514abb01161e96473168d931ab76aa8e570
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-18 18:49:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
 countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
 next_country = "Niger"
-updated_array = using_push(countries_in_western_africa, next_country)
+updated_array = countries_in_western_africa.push(next_country)
 expect(updated_array.last).to eq(next_country)
