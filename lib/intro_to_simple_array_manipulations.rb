1edee3552ead11dc54b17e9e83bc52cca3288619
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-18 18:35:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
    next_color = "violet"
-   colors_in_the_rainbow.push(next_color)
+   new_array = colors_in_the_rainbow.push(next_color)
    
  
